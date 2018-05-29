0a5c9b5545243a79f7e5564660b6933464120a41
Carrie Savinell <carrielane89@gmail.com>
2018-05-29 16:13:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,14 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
 
 class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
